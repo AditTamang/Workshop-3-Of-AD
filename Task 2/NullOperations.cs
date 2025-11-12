83f7114d9bea9f90df4e480c30126adbb88f421b
Adit Tamang <tamangadit86@gmail.com>
2025-11-12 10:04:49 +0545

    Task 1

---
+++ Task 2/NullOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class NullOperations
+    {
+       public void PerformNullCheck()
+        {
+            string userName = null;
+
+            string nullCheck = (userName == null) ? "Username is null" : userName;
+            Console.WriteLine(nullCheck);
+
+            string userName1 = null;
+
+            string nullCoalescing = (userName1 ?== null) ? "UserName is null" : userName1;
+
+
+        }
+    }
+}
